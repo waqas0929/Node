7ce8b309553d9021851be7af2ddb037820fa0df0
waqas0929 <waqasraza0929@gmail.com>
2023-04-28 12:45:47 +0500

    new update

--- db/User.js
+++ db/User.js
@@ -1,11 +1,109 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
+// Define the old schema for the user document
+const oldUserSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  gender: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Define a middleware function to update the old schema documents
+oldUserSchema.pre("validate", async function (next) {
+  const user = this;
+
+  // Check if the document matches the old schema
+  if (!user.previousPasswords) {
+    // Add any missing fields to the document
+    user.previousPasswords = [];
+  }
+
+  next();
+});
+
+// Define the new schema for the user document
 const userSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
   gender: String,
-  email: String,
-  password: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  previousPasswords: [
+    {
+      password: {
+        type: String,
+        required: true,
+      },
+
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
 });
+
+// Use the middleware function to update the documents on the fly
+userSchema.pre("validate", async function (next) {
+  const user = this;
+
+  // Check if the document matches the old schema
+  if (!user.previousPasswords) {
+    // Add any missing fields to the document
+    user.previousPasswords = [];
+  }
+
+  next();
+});
+
+// Define the pre-save hook to hash the password and save old password
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    const salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(user.password, salt);
+
+    //save old password
+    if (user.previousPasswords.length >= 5) {
+      user.previousPasswords.pop();
+    }
+    user.previousPasswords = [
+      { password: user.password, date: new Date() },
+      ...user.previousPasswords,
+    ];
+  }
+  next();
+});
+
+// Define the method to compare the password
+userSchema.methods.comparePassword = async function (password) {
+  try {
+    const isMatch = await bcrypt.compare(password, this.password);
+    return isMatch;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 module.exports = mongoose.model("users", userSchema);
--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const express = require("express");
 require("./db/config");
 const User = require("./db/User");
 const { authenticateToken } = require("./middleware/auth");
+const bcrypt = require("bcrypt");
 
 const Jwt = require("jsonwebtoken");
 const jwtKey = "e-com";
 
+const compareString = async (string, encryptedString) => {
+  await bcrypt.compare(string, encryptedString);
+};
+
 const app = express();
 
 app.use(express.json());
+
+const encryptString = async (string) => {
+  const salt = await bcrypt.genSalt(10);
+
+  return bcrypt.hash(string, salt);
+};
 
 app.post("/register", async (req, resp) => {
   try {
@@ -16,7 +27,11 @@ app.post("/register", async (req, resp) => {
     if (existingUser) {
       resp.send({ message: "Email is already register" });
     } else {
-      let user = new User(req.body);
+      const hashedPassword = await encryptString(req.body.password);
+      let user = new User({
+        email: req.body.email,
+        password: hashedPassword,
+      });
       let result = await user.save();
       resp.send(result);
     }
@@ -25,30 +40,106 @@ app.post("/register", async (req, resp) => {
   }
 });
 app.post("/login", async (req, resp) => {
-  if (req.body.email && req.body.password) {
-    let user = await User.findOne({
-      email: req.body.email,
-      password: req.body.password,
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({
+      email,
     });
-    if (user) {
-      Jwt.sign({ user }, jwtKey, (err, token) => {
-        resp.send({ user, auth: token });
-      });
-    } else {
-      resp.send({ result: "INVALID EMAIL OR PASSWORD" });
+    if (!user) {
+      return resp.status(401).send({ message: "Invalid email or password" });
     }
-  } else {
-    resp.send({ result: "Email and password are required" });
+    const isPasswordValid = await compareString(password, user.password);
+
+    if (!isPasswordValid) {
+      return resp.status(401).send({ message: "Invalid email or password" });
+    }
+
+    const token = Jwt.sign({ user }, jwtKey);
+    const userDetails = {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    };
+    resp.send({ userDetails, token });
+  } catch (error) {
+    resp.status(500).send({ message: error.message });
   }
 });
 
-app.get("/user/my", authenticateToken, async (req, resp) => {
+app.get("/user/my/password", authenticateToken, async (req, resp) => {
   try {
     let user = await User.findById(req.user.id);
     if (!user) {
       return resp.status(404).send({ message: "User Not Found" });
     }
     resp.send(user);
+  } catch (error) {
+    resp.status(500).send({ message: error.messagae });
+  }
+});
+
+app.put("/user/my/password", authenticateToken, async (req, resp) => {
+  try {
+    let user = await User.findById(req.user.id);
+
+    if (!user) {
+      return resp.status(404).send({ message: "User Not Found" });
+    }
+
+    // check if old password is entered correctly
+    if (!req.body.oldPassword || !req.body.newPassword) {
+      return resp
+        .status(400)
+        .send({ message: "Please Enter both old and new Password" });
+    }
+
+    const isOldPasswordCorrect = await compareString(
+      req.body.oldPassword,
+      user.password
+    );
+
+    if (!isOldPasswordCorrect) {
+      return resp.status(400).send({ message: "incorrect old password" });
+    }
+
+    //check if the new password is same as the old password
+    if (req.body.oldPassword === req.body.newPassword) {
+      return resp
+        .status(400)
+        .send({ message: "New Password conot be the same as old password" });
+    }
+
+    // Check if the new password has been used before
+
+    const previousPasswords = user.previousPasswords || [];
+    const isNewPasswordUsedBefore = previousPasswords.some((passwordObject) =>
+      bcrypt.compareSync(req.body.newPassword, passwordObject.password)
+    );
+
+    if (isNewPasswordUsedBefore) {
+      return resp
+        .status(400)
+        .send({ message: "new password has been used before" });
+    }
+
+    // Generate a new password
+
+    const hashedPassword = await encryptString(req.body.newPassword);
+
+    // Save the new password and update the previous passwords list
+
+    if (previousPasswords.length >= 12) {
+      previousPasswords.pop();
+    }
+    user.password = hashedPassword;
+    user.previousPasswords = [
+      { password: hashedPassword, date: new Date() },
+      ...previousPasswords,
+    ];
+    const result = await user.save();
+
+    resp.send({ message: "Password Changed Successfully" });
   } catch (error) {
     resp.status(500).send({ message: error.message });
   }
